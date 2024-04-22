8c7a1d384a6702d9b753aa428ff32913b52fc0d9
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-04-23 00:08:12 +0200

    Change code style

--- src/input.rs
+++ src/input.rs
@@ -10,8 +10,8 @@ pub fn update_input(
     piece_id : i32,
     rotation_id : &mut i32,
     rotate_hold : &mut bool,
-    game_over : &mut bool
-) {
+    game_over : &mut bool)
+{
     let key : i32 = getch();
     if key == 27 {
         *game_over = true;
@@ -22,7 +22,7 @@ pub fn update_input(
     } else if key == 258 { // D
         *y += if tetromino::does_piece_fit(field, piece_id, *rotation_id, *x, *y + 1) {1} else {0};
     } else if (key == 32) | (key == 259) { // U
-        *rotation_id += if *rotate_hold && tetromino::does_piece_fit(field, piece_id, *rotation_id + 1, *x, *y) {1} else {0};
+        *rotation_id += if *rotate_hold & tetromino::does_piece_fit(field, piece_id, *rotation_id + 1, *x, *y) {1} else {0};
         *rotate_hold = false;
     } else {
         *rotate_hold = true;
